1969eb97aa0de8b79627f029b6951de85ac11c24
LucasN <lucas.n230896@gmail.com>
2026-02-24 20:09:49 +0000

    feat: add user namespace to API initialization

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -5,6 +5,7 @@ Module initializing the Flask application and setting up the API
 
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 
 def create_app():
@@ -14,5 +15,6 @@ def create_app():
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews,
     # and amenities will be added later
+    api.add_namespace(users_ns, path='/api/v1/users')
 
     return app
