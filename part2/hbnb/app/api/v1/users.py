f8616b446cc91b84247de8b93630487de823edae
LucasN <lucas.n230896@gmail.com>
2026-02-24 20:50:29 +0000

    fix: correct import statement for facade in users API

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask_restx import Namespace, Resource, fields
-from app.services import facade
+from app.services.facade import facade
 
 api = Namespace('users', description='User operations')
 
