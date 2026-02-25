59962775644f8078f5e08a6c786d7ecf6a0dd9ee
Allix <thewitchonxanax@gmail.com>
2026-02-25 16:24:03 +0000

    feat: implement CRUD operations for amenities in HBnBFacade

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -12,7 +12,6 @@ from app.models.review import Review
 from app.models.amenity import Amenity
 
 
-
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -49,6 +48,28 @@ class HBnBFacade:
 
         self.user_repo.update(user_id, new_data)
         return user
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+
+        if 'name' in amenity_data:
+            amenity.name = amenity_data['name']
+
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return amenity
 
     def create_place(self, place_data):
         owner = self.user_repo.get(place_data["owner_id"])
@@ -105,6 +126,8 @@ class HBnBFacade:
                 raise ValueError("Invalid longitude")
             place.longitude = place_data["longitude"]
 
+        self.place_repo.update(place_id, place_data)
         return place
+
 
 facade = HBnBFacade()
