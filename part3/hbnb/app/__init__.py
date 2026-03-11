36729bf5aadcb9339b2fcf1e1e6f585b815ce6ff
LucasN-ux <lucas.n230896@gmail.com>
2026-03-11 14:36:31 +0100

    feat: Implement JWT Authentication and create token to reconyze user identity

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,21 +6,41 @@ Module initializing the Flask application and setting up the API
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
+    authorizations = {
+        'Bearer Auth': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization',
+            'description': "Add 'Bearer <JWT>'"
+        }
+    }
+
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        doc='/',
+        authorizations=authorizations
+    )
 
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
     # import debug namespace for user password operations
     from app.api.v1.users_debug import api_debug as users_debug_ns
 
@@ -28,6 +48,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     # add debug namespace for user password operations (not exposed in production)
     api.add_namespace(users_debug_ns, path='/api/v1/users_debug')
 
