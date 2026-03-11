f9bb1b0f55bb4f03bad6277c00c10c346ac063be
LucasN-ux <lucas.n230896@gmail.com>
2026-03-11 10:44:23 +0100

    fix: import namespaces in create_app to resolve the circular import

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,10 +6,6 @@ Module initializing the Flask application and setting up the API
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
 
 bcrypt = Bcrypt()
 
@@ -20,6 +16,11 @@ def create_app(config_class="config.DevelopmentConfig"):
     bcrypt.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
+
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
